f8b4edcc0838574c73cc457f7ccc881aab25ad00
Simon <simon.j.goring@gmail.com>
2014-03-25 12:31:11 -0500

    Forgot to close brackes in SourceRun.  Whoops.

--- R/SourceRun.R
+++ R/SourceRun.R
@@ -6,17 +6,17 @@ par <- FALSE
 
 file.lead <- ifelse(par == TRUE, 'R/', 'R/nonPar/')
 
-source(paste(file.lead, 'bootstrap_mat.R', sep ='')
+source(paste(file.lead, 'bootstrap_mat.R', sep =''))
 cat('Done MAT/n')
 
-source(paste(file.lead, 'bootstrap_wa.R', sep ='')
+source(paste(file.lead, 'bootstrap_wa.R', sep =''))
 cat('Done WA/n')
 
-source(paste(file.lead, 'bootstrap_wapls.R', sep ='')
+source(paste(file.lead, 'bootstrap_wapls.R', sep =''))
 cat('Done WAPLS/n')
 
-source(paste(file.lead, 'bootstrap_rfor.R', sep ='')
+source(paste(file.lead, 'bootstrap_rfor.R', sep =''))
 cat('Done rFor/n')
 
-source(paste(file.lead, 'bootstrap_brt.R', sep ='')
+source(paste(file.lead, 'bootstrap_brt.R', sep =''))
 cat('Done rFor/n')
